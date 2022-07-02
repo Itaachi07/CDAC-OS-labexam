8b69252bca506c81294affb007be14fb6da68a8c
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2022-07-02 15:28:13 +0530

    Question_2.sh complete

--- Question_2.sh
+++ Question_2.sh
@@ -2,17 +2,19 @@
 
 #creating menu
 
-echo Select choice 
-
-echo 1. ACCESS RIGHTS
-echo 2. COUNT OF FILES
-echo 3. PRINT LINES WITH .
-echo 4. CHARACTER COUNT
-echo 5. EXIT
-
-#while :
-#do
+while :
+do
+	echo " "
+	echo ***********MENU***********
+	echo 1. ACCESS RIGHTS
+	echo 2. COUNT OF FILES
+	echo 3. PRINT LINES WITH .
+	echo 4. CHARACTER COUNT
+	echo 5. EXIT
+	echo "**************************"	
+	echo " "
 	echo Enter your choice
+
 
 	read choice
 
@@ -22,22 +24,26 @@ echo 5. EXIT
 			read filename
 			ls -lh $filename
 			;;
-		2) echo Number of files in current directly 
+
+		2) echo -n  Number of files in current directly :
 			ls | wc -l
 			;;
+
 		3) echo Enter the file name 
 			read file
 			echo The lines with .
 		       grep "\.$" $file	
-		
 			;;
+
 		4) echo enter the file name
 		read file
 		echo -n The number of characters in $file are:
 			wc -m $file
-			;;	
+			;;
+
 		5) echo Exiting . . 
 			exit
 			;;
 		esac
+	done
 
